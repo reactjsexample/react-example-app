03842d9473906256140515c46f03d39e582d4c31
ReactJSExample <info@reactjs-example.com>
2019-08-04 21:08:56 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import './App.scss';
+import XxxAnswersPage from "./pages/XxxAnswersPage/XxxAnswersPage";
+import XxxHeader from './components/XxxHeader/XxxHeader';
+import XxxHomePage from "./pages/XxxHomePage/XxxHomePage";
+import XxxPageNotFoundPage from "./pages/XxxPageNotFoundPage/XxxPageNotFoundPage";
+import XxxQuestionsPage from "./pages/XxxQuestionsPage/XxxQuestionsPage";
+
+function App() {
+  return (
+      <div id="xxx-app">
+        <Router>
+          <XxxHeader/>
+          <div>
+            <Switch>
+              <Route exact path="/" component={XxxHomePage}/>
+              <Route path="/answers/:id" component={XxxAnswersPage}/>
+              <Route path="/questions" component={XxxQuestionsPage}/>
+              <Route component={XxxPageNotFoundPage}/>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+  );
+}
+
+export default App;
